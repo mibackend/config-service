4eacfbddf7cb341e4054f39f30af651f2443395e
helloImxy <1045776650@qq.com>
2024-06-21 18:45:00 +0800

    change

--- src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
+++ src/test/java/com/example/config_service/ConfigServiceApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.config_service;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConfigServiceApplicationTests {
 
-//	@Test
+	@Test
 	void contextLoads() {
 	}
 
